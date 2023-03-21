f21c8d7a61b621591b4edb0d50fea67bac15b317
Tien Dang <tien.thi.thuy.dang@mgm-tp.com>
2023-03-21 17:09:53 +0700

    Fix error of spring scanner

--- src/main/java/io/codeclou/kitchen/duty/rest/UserSearchResource2.java
+++ src/main/java/io/codeclou/kitchen/duty/rest/UserSearchResource2.java
@@ -1,11 +1,13 @@
 package io.codeclou.kitchen.duty.rest;
 
+import com.atlassian.jira.security.JiraAuthenticationContext;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.atlassian.plugins.rest.common.security.AnonymousAllowed;
 
-import javax.inject.Named;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -15,11 +17,19 @@ import org.springframework.stereotype.Component;
 @Path("/message")
 public class UserSearchResource2 {
 
+    private final JiraAuthenticationContext authContext;
+
+    @Autowired
+    public UserSearchResource2(@ComponentImport JiraAuthenticationContext authContext) {
+        this.authContext = authContext;
+    }
+
     @GET
     @AnonymousAllowed
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Response getMessage()
     {
+        authContext.getLoggedInUser();
        return Response.ok(new UserSearchResource2Model("Hello World")).build();
     }
 }
--- src/main/java/io/codeclou/kitchen/duty/webwork/KitchenDutyPlanningWebworkAction.java
+++ src/main/java/io/codeclou/kitchen/duty/webwork/KitchenDutyPlanningWebworkAction.java
@@ -4,7 +4,6 @@ import com.atlassian.jira.web.action.JiraWebActionSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-@Component
 public class KitchenDutyPlanningWebworkAction extends JiraWebActionSupport {
 
   private static final Logger log = LoggerFactory.getLogger(KitchenDutyPlanningWebworkAction.class);
